4c91f7b5bd4e0f232dac5ad8d6b631f3ab3ac02d
kandyelmo <43852025+kandyelmo@users.noreply.github.com>
2019-11-08 15:03:57 -0500

    Setting up Progress Bar and Core Loop of Turn State
    I've set up scaling on the ProgressBar and the way turns work into this system

--- CApstone-TBBS/Assets/Scripts/EnemyStateMachine.cs
+++ CApstone-TBBS/Assets/Scripts/EnemyStateMachine.cs
@@ -1,20 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyStateMachine : MonoBehaviour
 {
     public BaseEnemy enemy;
+
+    public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    }
+    public TurnState currentState;
+    //for the ProgressBar
+    private float cur_cooldown = 0f;
+    private float max_cooldown = 5f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = TurnState.PROCESSING;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case (TurnState.PROCESSING):
+                UpgradeProgressBar();
+                break;
+            case (TurnState.ADDTOLIST):
+                break;
+            case (TurnState.WAITING):
+                break;
+            case (TurnState.SELECTING):
+                break;
+            case (TurnState.ACTION):
+                break;
+            case (TurnState.DEAD):
+                break;
+        }
+    }
+
+    void UpgradeProgressBar()
+    {
+        cur_cooldown = cur_cooldown + Time.deltaTime;
+     
+        if (cur_cooldown >= max_cooldown)
+        {
+            currentState = TurnState.ADDTOLIST;
+        }
     }
 }
