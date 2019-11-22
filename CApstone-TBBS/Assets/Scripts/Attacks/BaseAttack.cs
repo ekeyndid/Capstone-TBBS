165025e4630255657e0f545e34e48d7a0763cb1a
AzurdaX <56734838+AzurdaX@users.noreply.github.com>
2019-11-22 15:19:25 -0500

    Video 11 Completed
    nyoom

---
+++ CApstone-TBBS/Assets/Scripts/Attacks/BaseAttack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseAttack
+{
+    public string AttackName;
+    public string AttackDesc;
+    public float Damage;
+    public float ManaCost; //if spell
+
+}
---
+++ CApstone-TBBS/Assets/Scripts/Attacks/Physical/Slash.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slash : BaseAttack
+{
+    public Slash()
+    {
+        AttackName = "Slash";
+        AttackDesc = "A basic slash with a sword, dagger, or alike weapon."
+        ManaCost = 0.0f;
+        Damage = 10.0f;
+    }
+}
